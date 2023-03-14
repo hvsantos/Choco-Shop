c2129b1c4cceea177161dcf854da0c4e0242a4c2
hvsantos <hv_santos@outlook.com>
2023-03-14 19:25:47 -0300

    feat add test if the chocolate list is returned

--- tests/integration/chocolates.test.js
+++ tests/integration/chocolates.test.js
@@ -10,6 +10,13 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
+const chocoList = [
+  { id: 1, name: 'Mint Intense', brandId: 1 },
+  { id: 2, name: 'White Coconut', brandId: 1 },
+  { id: 3, name: 'Mon Chéri', brandId: 2 },
+  { id: 4, name: 'Mounds', brandId: 3 },
+];
+
 describe('Teste o caminho /chocolates', () => {
   let resp;
 
@@ -19,5 +26,9 @@ describe('Teste o caminho /chocolates', () => {
 
   it('Retorna o status de 200', () => {
     expect(resp.status).to.be.equals(200);
+  });
+
+  it('Retorna a lista completa de chocolates', async () => {
+    expect(resp.body.chocolates).to.deep.equal(chocoList);
   });
 });
